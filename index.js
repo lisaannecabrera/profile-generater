1f44acffbb21c00ab4fee93e3f5a376301915ddf
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-01-27 16:23:25 -0800

    questions

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-'use strict'
+"use strict";
 
-const fs = require('fs');
-const inquirer = require('inquirer');
-const axios = require('axios')
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
 
+const userQuestions = [
+  {
+    type: "list",
+    name: "favoriteColor",
+    message: "What is your favorite color?",
+    choices: ["red", "blue", "green", "black"]
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username?"
+  }
+];
